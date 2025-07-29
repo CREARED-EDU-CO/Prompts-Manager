cbbe00228572f4abca52f2404d20e372816f2e63
CREARED <150289917+CREARED-EDU-CO@users.noreply.github.com>
2025-07-29 11:53:33 -0500

    Update i18n.js

--- js/i18n.js
+++ js/i18n.js
@@ -1,3 +1,22 @@
+/*
+ * Este archivo es parte del proyecto Prompts-Manager.
+ *
+ * Copyright (C) 2025 CREARED.EDU.CO
+ *
+ * Este programa es software libre: puedes redistribuirlo y/o modificarlo 
+ * bajo los términos de la Licencia Pública General Affero de GNU publicada 
+ * por la Free Software Foundation, ya sea la versión 3 de la Licencia, 
+ * o (a tu elección) cualquier versión posterior.
+ *
+ * Este programa se distribuye con la esperanza de que sea útil, 
+ * pero SIN NINGUNA GARANTÍA; sin incluso la garantía implícita de 
+ * COMERCIABILIDAD o IDONEIDAD PARA UN PROPÓSITO PARTICULAR. 
+ * Consulta la Licencia Pública General Affero de GNU para más detalles.
+ *
+ * Deberías haber recibido una copia de la Licencia Pública General Affero de GNU 
+ * junto con este programa. En caso contrario, consulta <https://www.gnu.org/licenses/agpl-3.0.html>.
+ */
+
 'use strict';
 
 // Centralización de mensajes de la app
